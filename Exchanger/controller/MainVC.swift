13f6b51606e103a50c017fe3d3be3d78d835aa10
Mahmudul Hasan <mhasan341@gmail.com>
2022-09-15 23:22:07 +0600

    Tested The Network Call

--- Exchanger/controller/MainVC.swift
+++ Exchanger/controller/MainVC.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import Combine
 
 class MainVC: UIViewController {
   // Stores my balance cards
   let balanceStack = UIStackView()
   // Currencies the app supports
 
+  // to store our cancellables
+  var cancellables = Set<AnyCancellable>()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.backgroundColor = .red
+    self.title = "Exchanger"
+    navigationController?.navigationBar.prefersLargeTitles = true
+
   }
+
+  func exchangeCurrencyOf(_ amount: String, from input: String, to output: String) {
+    let url = URL(string: "http://api.evp.lt/currency/commercial/exchange/\(amount)-\(input)/\(output)/latest")
+
+    URLSession.shared.dataTaskPublisher(for: url!)
+      .sink { error in
+        print(error)
+      } receiveValue: { response in
+        print(response)
+      }
+      .store(in: &cancellables)
+  }
+
+  
 }
---
+++ Exchanger/extension/MainVC+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  MainVC+Ext.swift
+//  Exchanger
+//
+//  Created by Mahmudul Hasan on 2022-09-15.
+//
+
+import Foundation
+
+extension MainVC {
+  
+}
